e61bf2007735685177d2f24df099e06096288933
wangguofeng <wangguofeng@diligrp.com>
2016-09-05 15:58:42 +0800

    add anymatching  class

---
+++ src/test/java/com/wangguofeng1923/tests/ArgMatching.java
@@ -0,0 +1,26 @@
+package com.wangguofeng1923.tests;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ArgMatching {
+	@Test
+	public void anyInt(){
+		ArrayList list=Mockito.mock(ArrayList.class);
+		Mockito.when(list.get(Mockito.anyInt())).thenReturn("mock object");
+		Assert.assertEquals(null, "mock object", list.get(0));
+		Assert.assertEquals(null, "mock object", list.get(100));
+	}
+	@Test
+	public void anyString(){
+		HashMap<String, String> map=Mockito.mock(HashMap.class);
+		Mockito.when(map.get(Mockito.anyString())).thenReturn("mock object");
+		Assert.assertEquals(null, "mock object", map.get("key"));
+		Assert.assertEquals(null, "mock object", map.get("mykey"));
+	}
+}
